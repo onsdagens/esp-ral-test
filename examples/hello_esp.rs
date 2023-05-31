6d47110867db85d00fad8bf339bcc4c7e5967e26
onsdagens <pawdzi-7@student.ltu.se>
2023-06-01 01:16:37 +0200

    cleanup

--- examples/hello_esp.rs
+++ examples/hello_esp.rs
@@ -2,13 +2,10 @@
 #![no_main]
 use rtt_target::{rtt_init_print, rprintln};
 use core::panic::PanicInfo;
-//use panic_rtt_target as _;
 use esp_riscv_rt::{entry};
-// /use gpio;
 #[entry]
 unsafe fn main()->!{
     rtt_init_print!();
-   // let gpio = gpio::take().unwrap();
     rprintln!("Hi!");
     loop{continue;}
 }
